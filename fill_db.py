6e15ffe39c336cc27f5e771708aa9dff3be2f79d
Ricardo Alanis <ricardo.alanis@gmail.com>
2016-01-15 01:06:32 -0600

    added fix to chosen bug

--- fill_db.py
+++ fill_db.py
@@ -112,6 +112,7 @@ def responses_per_year(year_string, variable_name, filtered_data, responses_vari
             try:
                 yearly_sum[responses_variable[variable_name][key]] = str(yearly_responses[key])
             except:
+                print(variable_name)
                 print(responses_variable[variable_name])
                 print(key)
                 print(yearly_responses)
